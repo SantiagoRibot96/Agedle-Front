0884400684e6910f97ed45b6961e9d594df3b3c4
SantiagoRibot96 <85650377+santiagoribot@users.noreply.github.com>
2025-10-14 17:36:32 -0300

    cambios en la db

--- agedle/src/Configs/config.js
+++ agedle/src/Configs/config.js
@@ -1,4 +1,4 @@
 const config = {
-  url: import.meta.env.VITE_API_URL || "http://18.232.128.55:8080/api",
+  url: import.meta.env.VITE_API_URL || "http://localhost:8080/api",
 };
 export default config;
